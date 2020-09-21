28e0d08e232f8c00f74c43b4aa937265560a0cc8
Douglas Gass <douglasgass2@hotmail.com>
2020-09-21 19:46:36 -0300

    Estrutura inicial

---
+++ src/main/java/com/mobilidade/MobilidadeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mobilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilidadeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilidadeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobilidade/MobilidadeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilidadeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
